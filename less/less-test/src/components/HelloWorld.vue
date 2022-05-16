447e28cbfa316e7e64862e4fec747556453b69e2
GiorunoGiovanna <971615621@qq.com>
2022-05-16 16:00:59 +0800

    init

---
+++ less/less-test/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="a">
+    <div class="b"></div>
+    <div class="c"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
+
+<style lang="css" scoped>
+/* @import url(../less/helloworld.css); */
+@import url(../less/mixin.css);
+</style>
